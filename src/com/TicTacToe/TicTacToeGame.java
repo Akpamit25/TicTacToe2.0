35bc13cfa350fd10ba98b59419efe0805e7ac1ee
Amit Kumar <amitnitsri25@gmail.com>
2020-10-05 19:36:05 +0530

    Master Created

--- src/com/TicTacToe/TicTacToeGame.java
+++ src/com/TicTacToe/TicTacToeGame.java
@@ -5,31 +5,9 @@ import java.lang.*;
 public class TicTacToeGame {
 
 	public static void main(String[] args) {
-	   char[] board;
-	   board = createBoard();
+	 System.out.println("Welcome To Tic Tac Toe, Let's Play !!");
 
 	}
-	
-	
-	
-	private static char[] createBoard()
-	{
-		char[] board1 = new char[10];
-		for(int i=0;i<board1.length;i++)
-		{
-			board1[i]=' ';
-		}
-		return board1;
-	}
+
 
 }
-
-
-
-
-
-
-
-
-
-// UC1 : To create a 
