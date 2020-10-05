f87c676b5cdcf99b946ace5e7f1fb3270e5094ce
Amit Kumar <amitnitsri25@gmail.com>
2020-10-05 22:12:44 +0530

    UC2 Done

--- src/com/TicTacToe/TicTacToeGame.java
+++ src/com/TicTacToe/TicTacToeGame.java
@@ -8,14 +8,28 @@ public class TicTacToeGame {
 	public static void main(String[] args) {
 		char[] board;
 		board = createBoard();
+		System.out.println("Enter Your Mark");
+		Scanner sc = new Scanner(System.in);
+		char userMark = userChoice(sc);
+		char computerMark = (userMark == 'X') ? 'O' : 'X';
 
 	}
 
-	private static char[] createBoard() {
+	private static char[] createBoard()  ////CREATE Array
+	{
 		char[] board1 = new char[10];
 		for (int i = 0; i < board1.length; i++) {
 			board1[i] = ' ';
 		}
 		return board1;
 	}
+	
+	private static char userChoice(Scanner sc1)
+	{
+		char c = sc1.next().toUpperCase().charAt(0);
+		return c;
+	}
+	
+	
+	
 }
